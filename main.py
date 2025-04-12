d41fe94869216c4164414aad3d2166995950d28e
Ikki <ikki.vdm05@gmail.com>
2025-04-12 07:35:57 +0200

    Start new model and update requirements

--- main.py
+++ main.py
@@ -1,42 +1,13 @@
+import os
+import keras
+import numpy as np
 import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
-from torch.utils.data import DataLoader
-from dataloader import MovieLensDataset
-from model import MatrixFactorization
-import torch.nn as nn
-import torch
+from sklearn.model_selection import train_test_split
 
 
-ratings = pd.read_csv('data/ratings.csv')
-movies = pd.read_csv('data/movies.csv')
-
-user_encoder = LabelEncoder()
-movie_encoder = LabelEncoder()
-
-ratings['user'] = user_encoder.fit_transform(ratings['userId'])
-ratings['movie'] = movie_encoder.fit_transform(ratings['movieId'])
-
-dataset = MovieLensDataset(ratings)
-
-
-model = MatrixFactorization(
-    num_users=ratings['user'].nunique(),
-    num_movies=ratings['movie'].nunique()
-)
-
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-loss_fn = nn.MSELoss()
-
-dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
-
-for epoch in range(10):
-    model.train()
-    total_loss = 0
-    for users, movies, ratings_batch in dataloader:
-        preds = model(users, movies)
-        loss = loss_fn(preds, ratings_batch)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-    print(f"Epoch {epoch+1}: Loss = {total_loss:.4f}")
+data_movies = pd.read_csv("data/movies.csv")
+data_ratings = pd.read_csv("data/ratings.csv")
+print(data_movies)
