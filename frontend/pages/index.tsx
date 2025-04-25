12a3b92598e6e493e2e8443e3e594f4a0857e2f9
Ikki <ikki.vdm05@gmail.com>
2025-04-25 14:53:36 +0200

    Added the right dependencies for the frontend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,8 @@
 const homePage = () => {
-  return <p>Coming soon i guess</p>;
+  return (
+    <main>
+      <p className="pl-6 text-4xl text-gray-800">Coming soon</p>
+    </main>
+  );
 };
 export default homePage;
